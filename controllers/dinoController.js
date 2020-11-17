b4782224f9622fcfdc1084839a2b9216679d2969
sschneeberg <simone.m.schneeberg@gmail.com>
2020-11-17 11:37:13 -0500

    Add post route

--- controllers/dinoController.js
+++ controllers/dinoController.js
@@ -1,6 +1,12 @@
 const dinoRouter = require('express').Router();
 const fs = require('fs'); //fs for file system
 
+function readDinos() {
+    const dinoData = fs.readFileSync('./dinosaurs.json');
+    const dinos = JSON.parse(dinoData);
+    return dinos;
+}
+
 dinoRouter.get('/', (req, res) => {
     const dinoData = fs.readFileSync('./dinosaurs.json');
     const dinos = JSON.parse(dinoData);
@@ -8,13 +14,27 @@ dinoRouter.get('/', (req, res) => {
 
 })
 
+dinoRouter.get('/new', (req, res) => {
+    res.render('dinosaurs/new')
+})
+
 dinoRouter.get('/:id', (req, res) => {
-    const dinoData = fs.readFileSync('./dinosaurs.json');
-    const dinos = JSON.parse(dinoData);
+    const dinos = readDinos()
     const index = parseInt(req.params.id);
     const dino = dinos[index];
     res.render('dinosaurs/show', { dino });
 
 })
+
+dinoRouter.post('/', (req, res) => {
+    const dinos = readDinos()
+    const newDino = req.body;
+    dinos.push(newDino);
+    fs.writeFileSync('./dinosaurs.json', JSON.stringify(dinos));
+    //make a new GET request
+    res.redirect('/dinosaurs');
+})
+
+
 
 module.exports = dinoRouter;
--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const expressEjsLayouts = require('express-ejs-layouts');
 const app = express();
 
 app.use(expressEjsLayouts);
-
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
+//body-parser
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
     res.render('index');
