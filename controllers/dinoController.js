0b5929e8b617dcc1a592c56cb457fd96260e946f
sschneeberg <simone.m.schneeberg@gmail.com>
2020-11-17 11:15:23 -0500

    Add index route

--- controllers/dinoController.js
+++ controllers/dinoController.js
@@ -1,7 +1,10 @@
 const dinoRouter = require('express').Router();
+const fs = require('fs'); //fs for file system
 
 dinoRouter.get('/', (req, res) => {
-    res.render('dinosaurs/index');
+    const dinoData = fs.readFileSync('./dinosaurs.json');
+    const dinos = JSON.parse(dinoData);
+    res.render('dinosaurs/index', { dinos });
 
 })
 
