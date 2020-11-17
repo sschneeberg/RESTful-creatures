7d594dd179a67e486d53e8e62c49e1396bfdf4b9
sschneeberg <simone.m.schneeberg@gmail.com>
2020-11-17 11:20:26 -0500

    Add show route

--- controllers/dinoController.js
+++ controllers/dinoController.js
@@ -8,4 +8,13 @@ dinoRouter.get('/', (req, res) => {
 
 })
 
+dinoRouter.get('/:id', (req, res) => {
+    const dinoData = fs.readFileSync('./dinosaurs.json');
+    const dinos = JSON.parse(dinoData);
+    const index = parseInt(req.params.id);
+    const dino = dinos[index];
+    res.render('dinosaurs/show', { dino });
+
+})
+
 module.exports = dinoRouter;
